4a011e0f4cf462403576e59a4b576e1fa3e474a5
Okiushi <100536875+Okiushi@users.noreply.github.com>
2022-03-11 18:09:29 +0100

    0.0.7 (navigation par page dans l'IHM)
    + Ajout d'un system de page à l'IHM.

--- code/ihm.py
+++ code/ihm.py
@@ -1,6 +1,8 @@
 # --- Imporations des Module
 
+from secrets import choice
 from data import *
+from tkinter import ttk # Pour une UI conforme au dernière version du système d'exploitations
 
 # --- Affectation des variables global
 
@@ -8,13 +10,43 @@ globalSize = 1
 case_size = 40
 cases = []
 
-def creatgrid():
+# --- Ajout de la bar d'onglet
+tabBar = ttk.Frame(app,height = 10)
+tabBar.pack(expand = 1, fill ="both")
+
+# --- Création des pages :
+
+# MainMenu
+mainMenuPage = ttk.Frame(tabBar)
+
+# MainParty
+mainPartyPage = ttk.Frame(tabBar,height = 300)
+
+# Start
+startPage = ttk.Frame(tabBar)
+
+# MainSettings
+mainSettingsPage = ttk.Frame(tabBar)
+
+# --- Élément de la fenêtre
+
+# MainMenu
+def itemMainMenuPage():
+    bouton_play = ttk.Button(mainMenuPage, text='Play',command=lambda:[mainMenuPage.pack_forget(),mainPartyPage.pack()])
+    bouton_play.grid(row = 1, column = 0, padx=3, pady=3)
+
+# MainParty
+def itemMainPartyPage():
+    app.title("Battleship - Party")
+    mapZone = Canvas(mainPartyPage, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
+    mapZone.grid(row = 0, column = 0, padx=3, pady=3)
     for map in range(mapNumber):
-        gridecase = Canvas(app, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize, bg = "lightblue")
         if map == playerMap-1:
-            gridecase.grid(row = 1, column = 0,columnspan=mapNumber, padx=3, pady=3)
+            gridecase = Canvas(mapZone, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
+            gridecase.grid(row = int(map/5) + 2, column = 0,columnspan=mapNumber, padx=100, pady=3)
         else:
-            gridecase.grid(row = 0, column = map, padx=3, pady=3)
+            gridecase = Canvas(mapZone, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
+            gridecase.grid(row = int(map/5), column = map , padx=3, pady=3)
         for ligne in range(mapSize):
             cases_ligne=[]
             for colonne in range(mapSize):
@@ -31,25 +63,17 @@ def creatgrid():
                 elif mapRead(map+1,j+1,i+1)[0] != "--":
                     gridecase.itemconfigure(cases[i][j], outline = "gainsboro",fill="lavender")
                 else:
-                    gridecase.itemconfigure(cases[i][j], outline = "steelblue",fill="steelblue3")
+                    gridecase.itemconfigure(cases[i][j], outline = "steelblue",fill="steelblue3")    
 
-# --- Élément de la fenêtre
+    bouton_menu = ttk.Button(mainPartyPage, text='Menu',command=lambda:[mainPartyPage.pack_forget(),mainMenuPage.pack()])
+    bouton_menu.grid(row = 1, column = 0, padx=3, pady=3)
 
+# --- Main
 def battleship():
     app.title("Battleship")
-    creatgrid()
-    menubar = Menu(app)
-    app.config(menu=menubar)
-    menu_selection = Menu(menubar,tearoff=0)
-    menubar.add_cascade(label="Jouer", menu=menu_selection)
+    # Initialisation des éléments des onglets
+    itemMainMenuPage()
+    itemMainPartyPage()
 
-    menu_selection.add_command(label="Standard contre l'IA ")
-    menu_selection.add_command(label="Standard contre un joueur")
-    menu_selection.add_command(label="Personalisé")
-
-    menu_settings= Menu(menubar,tearoff=0)
-    menubar.add_cascade(label= "Réglage", menu=menu_settings)
-    menu_stats= Menu(menubar,tearoff=0)
-    menubar.add_cascade(label= "Stats", menu=menu_stats)
-    menu_credit= Menu(menubar,tearoff=0)
-    menubar.add_cascade(label= "Support", menu=menu_credit)
+    # Ouverture du menu
+    mainMenuPage.pack()
