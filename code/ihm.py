344ccf2841286fbd3a6bf1a44eaaf733af4110aa
Okiushi <okiushi.ldb.private@gmail.com>
2022-03-12 02:39:15 +0100

    0.0.8 (taille de fenêtre)
    + Ouverture centré de la fenêtre
    + Taille prédéfinie de la fenêtre
    + Taille minimale de la fenêtre
    + Taille maximal de la fenêtre

--- code/ihm.py
+++ code/ihm.py
@@ -1,52 +1,65 @@
 # --- Imporations des Module
 
 from secrets import choice
+from turtle import bgcolor, color
 from data import *
 from tkinter import ttk # Pour une UI conforme au dernière version du système d'exploitations
 
-# --- Affectation des variables global
+
+# --- Affectation des variables
+
+import tkinter as tk
+
 
 globalSize = 1
 case_size = 40
 cases = []
 
 # --- Ajout de la bar d'onglet
-tabBar = ttk.Frame(app,height = 10)
-tabBar.pack(expand = 1, fill ="both")
+window = ttk.Frame(app,height = 10)
+window.pack(expand = 1, fill ="both")
 
 # --- Création des pages :
 
 # MainMenu
-mainMenuPage = ttk.Frame(tabBar)
+mainMenuPage = ttk.Frame(window)
 
 # MainParty
-mainPartyPage = ttk.Frame(tabBar,height = 300)
+mainPartyPage = ttk.Frame(window)
 
 # Start
-startPage = ttk.Frame(tabBar)
+startPage = ttk.Frame(window)
 
 # MainSettings
-mainSettingsPage = ttk.Frame(tabBar)
+mainSettingsPage = ttk.Frame(window)
 
 # --- Élément de la fenêtre
 
 # MainMenu
 def itemMainMenuPage():
-    bouton_play = ttk.Button(mainMenuPage, text='Play',command=lambda:[mainMenuPage.pack_forget(),mainPartyPage.pack()])
-    bouton_play.grid(row = 1, column = 0, padx=3, pady=3)
-
+    menuZone = ttk.Frame(mainMenuPage)
+    menuZone.pack()
+    bouton_play = ttk.Button(menuZone, text='Play',command=lambda:[mainMenuPage.pack_forget(),mainPartyPage.pack()])
+    bouton_play.grid(row = 1, column = 0, ipadx=150, ipady=20)
+    bouton_play = ttk.Button(menuZone, text='Settings',command=lambda:[mainMenuPage.pack_forget(),mainPartyPage.pack()])
+    bouton_play.grid(row = 2, column = 0, ipadx=150, ipady=20)
+    bouton_play = ttk.Button(menuZone, text='Exit',command=lambda:app.destroy())
+    bouton_play.grid(row = 3, column = 0, ipadx=150, ipady=20,sticky="")
+    
 # MainParty
 def itemMainPartyPage():
     app.title("Battleship - Party")
-    mapZone = Canvas(mainPartyPage, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
+    mapZone = ttk.Frame(mainPartyPage, width = 100*globalSize, height = 100*globalSize)
     mapZone.grid(row = 0, column = 0, padx=3, pady=3)
+    mapAdversZone = ttk.Frame(mapZone, width = 100*globalSize, height = 100*globalSize)
+    mapAdversZone.grid(row = 0, column = 0, padx=3, pady=3)
     for map in range(mapNumber):
         if map == playerMap-1:
             gridecase = Canvas(mapZone, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
             gridecase.grid(row = int(map/5) + 2, column = 0,columnspan=mapNumber, padx=100, pady=3)
         else:
-            gridecase = Canvas(mapZone, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
-            gridecase.grid(row = int(map/5), column = map , padx=3, pady=3)
+            gridecase = Canvas(mapAdversZone, width = (mapSize*case_size+2)*globalSize, height = (mapSize*case_size+2)*globalSize)
+            gridecase.grid(row = int(map/5), column = map , padx=3, pady=3,)
         for ligne in range(mapSize):
             cases_ligne=[]
             for colonne in range(mapSize):
@@ -71,6 +84,15 @@ def itemMainPartyPage():
 # --- Main
 def battleship():
     app.title("Battleship")
+
+    # Taille de la fenêtre
+    app.minsize(800, 450)
+    app.maxsize(app.winfo_screenwidth(),app.winfo_screenheight())
+    w  = 800
+    h = 450
+    x = (app.winfo_screenwidth()/2) - (w/2)
+    y = (app.winfo_screenheight()/2) - (h/2)
+    app.geometry('%dx%d+%d+%d' % (w, h, x, y))
     # Initialisation des éléments des onglets
     itemMainMenuPage()
     itemMainPartyPage()
